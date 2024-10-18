793e8b267dee9f40e1ce81185e8c7f80dbf747b3
Hoki Attila <hoki.attila@gmail.com>
2024-10-18 18:34:07 +0200

    Add validation

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f680dfb1ff5d8eeec3dab87a5ffd9c6b1a5a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f7bcfb9d82f55ee02888115a5834cfa8b8027f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
